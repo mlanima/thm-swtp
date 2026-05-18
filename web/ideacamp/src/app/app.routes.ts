8e47ebd651bb10e95791d159e07dbb952db6f7ce
Chris <heinzchris1@gmx.de>
2026-05-18 20:27:52 +0200

    Revert "feat: add layout and styling for createProject page."
    This reverts commit a8cc8a2cb9099c6fb5773a5106f4e73399884562.

--- web/ideacamp/src/app/app.routes.ts
+++ web/ideacamp/src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import {ProjectCreate} from '../pages/project-create/project-create';
 
-export const routes: Routes = [
-  {path: 'createProject', component: ProjectCreate},
-];
+export const routes: Routes = [];
--- web/ideacamp/src/components/createProject/wizzard-layout/wizard-layout.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-wizard-layout',
-  imports: [],
-  templateUrl: './wizard-layout.html',
-})
-export class WizardLayout {}
