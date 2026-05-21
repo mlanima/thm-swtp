59c0f6cc6a6545ab9bc6451046f7d09945829577
Daniel Schmik <daniel.schmik@mni.thm.de>
2026-05-21 14:48:11 +0200

    Eingabevalidierung + Loadingstate

--- web/ideacamp/src/pages/login/login.ts
+++ web/ideacamp/src/pages/login/login.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  emailOrName = '';
+  password = '';
+
+  submitted = false;
+  loading = false;
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (!this.emailOrName || !this.password) {
+      return;
+    }
+
+    this.loading = true;
+  }
+}
