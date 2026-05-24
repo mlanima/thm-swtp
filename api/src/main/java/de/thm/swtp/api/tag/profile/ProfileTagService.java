e3aa826fb67c7a18c02555742b45ac2c0a0966e9
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-05-24 16:16:45 +0200

    add transac on post profile tag

--- api/src/main/java/de/thm/swtp/api/tag/profile/ProfileTagService.java
+++ api/src/main/java/de/thm/swtp/api/tag/profile/ProfileTagService.java
@@ -8,6 +8,7 @@ import de.thm.swtp.api.tag.dto.AddProfileTagRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
@@ -17,6 +18,7 @@ public class ProfileTagService {
     private final TagRepository tagRepository;
     private final ProfileRepository profileTagRepository;
 
+    @Transactional
     public Tag addTagToProfile(String profileId, AddProfileTagRequest request) {
         Profile profile = profileTagRepository.findById(profileId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile not found"));
