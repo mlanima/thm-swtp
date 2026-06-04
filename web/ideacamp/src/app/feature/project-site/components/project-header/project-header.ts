f690b327cf0b5ad1cb6833bb3227255379c5cc29
Daniel Schmik <daniel.schmik@mni.thm.de>
2026-06-04 16:00:36 +0200

    changed position for settings button

--- web/ideacamp/src/app/feature/project-site/components/project-header/project-header.ts
+++ web/ideacamp/src/app/feature/project-site/components/project-header/project-header.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { ProjectResponse } from '../../../../models/project.model';
+import {AuthService} from '../../../auth/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './project-header.html'
 })
 export class ProjectHeader {
   @Input({ required: true }) project!: ProjectResponse;
-  @Input() isOwner = false;
+  //@Input() isOwner = false;
+
+  private readonly authService = inject(AuthService);
+
+  get isOwner(): boolean {
+    const user = this.authService.user();
+    if (!user || !this.project) {
+      return false;
+    }
+    return user.id === this.project.ownerId;
+  }
 }
