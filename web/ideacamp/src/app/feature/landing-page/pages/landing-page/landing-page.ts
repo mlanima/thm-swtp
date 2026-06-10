0f985505cec295f00f2b2ef9711c04c89057b5c0
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-06-10 19:39:54 +0200

    add link, fix margins, add hover

--- web/ideacamp/src/app/feature/landing-page/pages/landing-page/landing-page.ts
+++ web/ideacamp/src/app/feature/landing-page/pages/landing-page/landing-page.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   templateUrl: './landing-page.html',
 })
-export class LandingPage {}
+export class LandingPage {
+  auth = inject(AuthService);
+}
