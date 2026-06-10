6540990fc51c8a712063c5135006f0d89d346750
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-06-10 19:59:01 +0200

    remove confirmation from privacy settings

--- web/ideacamp/src/app/feature/project-settings/tabs/privacy-tab/privacy-tab.ts
+++ web/ideacamp/src/app/feature/project-settings/tabs/privacy-tab/privacy-tab.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnInit, computed, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { forkJoin, Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+import { Component, Input, OnInit, inject, signal } from '@angular/core';
 import { ProjectResponse } from '../../../../models/project.model';
 import { ProjectService } from '../../../project-site/project.service';
 
 @Component({
   selector: 'app-privacy-tab',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './privacy-tab.html',
 })
 export class PrivacyTab implements OnInit {
@@ -20,69 +17,46 @@ export class PrivacyTab implements OnInit {
   joinRequestsAllowed = signal(true);
   isSaving = signal(false);
 
-  private originalIsPublic = true;
-  private originalJoinRequestsAllowed = true;
-
-  hasChanges = computed(
-    () =>
-      this.isPublic() !== this.originalIsPublic ||
-      this.joinRequestsAllowed() !== this.originalJoinRequestsAllowed,
-  );
-
   ngOnInit(): void {
-    this.originalIsPublic = !this.project.isPrivateProject;
-    this.originalJoinRequestsAllowed = this.project.allowJoinRequests;
-    this.isPublic.set(this.originalIsPublic);
-    this.joinRequestsAllowed.set(this.originalJoinRequestsAllowed);
+    this.isPublic.set(!this.project.isPrivateProject);
+    this.joinRequestsAllowed.set(this.project.allowJoinRequests);
   }
 
   togglePublicVisibility(): void {
-    this.isPublic.set(!this.isPublic());
+    const newValue = !this.isPublic();
+    this.isPublic.set(newValue);
+    this.isSaving.set(true);
+
+    this.projectService
+      .updateProject(this.project.id, {
+        name: this.project.name,
+        description: this.project.description,
+        shortDescription: this.project.shortDescription ?? undefined,
+        projectUrl: this.project.projectUrl,
+        isPrivateProject: !newValue,
+      })
+      .subscribe({
+        next: () => this.isSaving.set(false),
+        error: () => {
+          this.isPublic.set(!newValue);
+          this.isSaving.set(false);
+        },
+      });
   }
 
   toggleAllowJoinRequests(): void {
-    this.joinRequestsAllowed.set(!this.joinRequestsAllowed());
-  }
-
-  saveSettings(): void {
-    if (!this.hasChanges() || this.isSaving()) return;
-
+    const newValue = !this.joinRequestsAllowed();
+    this.joinRequestsAllowed.set(newValue);
     this.isSaving.set(true);
 
-    const calls: Observable<ProjectResponse>[] = [];
-
-    if (this.isPublic() !== this.originalIsPublic) {
-      calls.push(
-        this.projectService.updateProject(this.project.id, {
-          name: this.project.name,
-          description: this.project.description,
-          shortDescription: this.project.shortDescription ?? undefined,
-          projectUrl: this.project.projectUrl,
-          isPrivateProject: !this.isPublic(),
-        }),
-      );
-    }
-
-    if (this.joinRequestsAllowed() !== this.originalJoinRequestsAllowed) {
-      calls.push(
-        this.projectService.updateAllowJoinRequests(
-          this.project.id,
-          this.joinRequestsAllowed(),
-        ),
-      );
-    }
-
-    forkJoin(calls).pipe(
-      finalize(() => this.isSaving.set(false)),
-    ).subscribe({
-      next: () => {
-        this.originalIsPublic = this.isPublic();
-        this.originalJoinRequestsAllowed = this.joinRequestsAllowed();
-      },
-      error: () => {
-        this.isPublic.set(this.originalIsPublic);
-        this.joinRequestsAllowed.set(this.originalJoinRequestsAllowed);
-      },
-    });
+    this.projectService
+      .updateAllowJoinRequests(this.project.id, newValue)
+      .subscribe({
+        next: () => this.isSaving.set(false),
+        error: () => {
+          this.joinRequestsAllowed.set(!newValue);
+          this.isSaving.set(false);
+        },
+      });
   }
 }
