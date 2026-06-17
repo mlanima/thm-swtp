f5e9ff7694c03b9082570e69b00318c58e76cb50
Torsten Schalz <torsten.schalz@mni.thm.de>
2026-06-17 09:47:42 +0200

    cleanup

--- api/src/main/java/de/thm/swtp/api/ApiApplication.java
+++ api/src/main/java/de/thm/swtp/api/ApiApplication.java
@@ -3,7 +3,6 @@ package de.thm.swtp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Trigger PR review build (retry)
 @SpringBootApplication
 public class ApiApplication {
 
