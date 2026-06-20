b4d6366f5f028668f0ee4875ec6bdc005c1f4241
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-06-20 16:39:45 +0200

    add settings route and integrate footer in app layout

--- web/ideacamp/src/app/app.ts
+++ web/ideacamp/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
