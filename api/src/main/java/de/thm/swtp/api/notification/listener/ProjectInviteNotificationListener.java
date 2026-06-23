2414c2e14856285fbfb41291d4aaa36b69f25999
Torsten Schalz <torsten.schalz@mni.thm.de>
2026-06-23 15:08:12 +0200

    refactor: remove redundant try/catch in ProjectInviteNotificationListener

--- api/src/main/java/de/thm/swtp/api/notification/listener/ProjectInviteNotificationListener.java
+++ api/src/main/java/de/thm/swtp/api/notification/listener/ProjectInviteNotificationListener.java
@@ -3,7 +3,6 @@ package de.thm.swtp.api.notification.listener;
 import de.thm.swtp.api.notification.event.ProjectInviteCreatedEvent;
 import de.thm.swtp.api.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -11,7 +10,6 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class ProjectInviteNotificationListener {
 
     private final NotificationService notificationService;
@@ -19,10 +17,6 @@ public class ProjectInviteNotificationListener {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onProjectInviteCreated(ProjectInviteCreatedEvent event) {
-        try {
-            notificationService.sendInviteMail(event);
-        } catch (Exception e) {
-            log.warn("Invite notification failed for {}: {}", event.invitedUserEmail(), e.getMessage());
-        }
+        notificationService.sendInviteMail(event);
     }
 }
