1fdc20b46ba78d8705e8346e3928aff7809b9f75
Chris <heinzchris1@gmx.de>
2026-06-30 20:41:26 +0200

    fix: adds mock testing for prof req component.

--- web/ideacamp/src/app/feature/moderator/professor-request/professor-request.component.spec.ts
+++ web/ideacamp/src/app/feature/moderator/professor-request/professor-request.component.spec.ts
@@ -1,14 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs'
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { ProfessorRequestComponent } from './professor-request.component';
+import { ModeratorProfessorRequestService } from './service/moderator-professor-request.service';
 
 describe('ProfessorRequestComponent', () => {
   let component: ProfessorRequestComponent;
   let fixture: ComponentFixture<ProfessorRequestComponent>;
 
+  const professorRequestServiceMock = {
+    getRequests: () =>
+      of({
+        content: [],
+        totalElements: 0,
+        totalPages: 0,
+        size: 20,
+        number: 0,
+      }),
+    acceptRequest: () => of({}),
+    rejectRequest: () => of({}),
+  };
+
+  const translateServiceMock = {
+    instant: (key: string) => key,
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProfessorRequestComponent],
+      providers: [
+        {
+          provide: ModeratorProfessorRequestService,
+          useValue: professorRequestServiceMock,
+        },
+        {
+          provide: TranslateService,
+          useValue: translateServiceMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfessorRequestComponent);
