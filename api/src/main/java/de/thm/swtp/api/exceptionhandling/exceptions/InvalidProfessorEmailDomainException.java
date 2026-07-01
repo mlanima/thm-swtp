ecbdb839f9c5dd08cd57dc61fb94892e810277b4
Chris <heinzchris1@gmx.de>
2026-07-01 21:08:33 +0200

    feat: adds scheduling and adds new custom exception.

---
+++ api/src/main/java/de/thm/swtp/api/exceptionhandling/exceptions/InvalidProfessorEmailDomainException.java
@@ -0,0 +1,7 @@
+package de.thm.swtp.api.exceptionhandling.exceptions;
+
+public class InvalidProfessorEmailDomainException extends RuntimeException {
+    public InvalidProfessorEmailDomainException(String email) {
+        super("Professor requests require a valid THM email address: " + email);
+    }
+}
