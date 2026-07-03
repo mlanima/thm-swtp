a42cc5c629599d4a768b62e19e3f99d5bacd2fe0
Chris <heinzchris1@gmx.de>
2026-07-03 18:13:20 +0200

    feat: adds domain model for reports.

---
+++ api/src/main/java/de/thm/swtp/api/reports/domain/Report.java
@@ -0,0 +1,30 @@
+package de.thm.swtp.api.reports.domain;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/** Domain model representing a report from a user. Reports are reviewed by moderators.
+ * A report can target different types, see {@link ReportTarget}. The target is identified
+ * by the combination of {@link ReportTarget} and {@code targetId}
+ */
+@Builder
+@Value
+public class Report {
+    UUID id;
+    UUID reporterId;
+    String reporterUsername;
+    ReportTarget target;
+    UUID targetId;
+    ReportReason reason;
+    String message;
+    ReportStatus status;
+    UUID reviewerKeycloakId;
+    String reviewerUsername;
+    LocalDateTime reviewedAt;
+    String moderatorMessage;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
