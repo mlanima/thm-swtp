c6d4bff624eaa671a9d187026dc611126aa7e1bf
Danylo Konovalenko <danylokonovalenko49@gmail.com>
2026-07-04 21:55:45 +0200

    remove unused Stream import in GooglePlacesClient

--- api/src/main/java/de/thm/swtp/api/location/GooglePlacesClient.java
+++ api/src/main/java/de/thm/swtp/api/location/GooglePlacesClient.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 @Slf4j
