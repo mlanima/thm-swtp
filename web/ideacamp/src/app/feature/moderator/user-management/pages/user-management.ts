eb88a9191f8e1f06cdbf1c6082a1fae9cc244757
Chris <heinzchris1@gmx.de>
2026-07-12 17:17:37 +0200

    fix: removes trailing coma.

--- web/ideacamp/src/app/feature/moderator/user-management/pages/user-management.ts
+++ web/ideacamp/src/app/feature/moderator/user-management/pages/user-management.ts
@@ -48,11 +48,11 @@ export class UserManagement implements OnInit {
   readonly isBanSubmitting = signal(false);
   readonly banErrorMessage = signal<string | null>(null);
 
-  private readonly activePageRange = createPageRange(this.activeUsers, this.activeCurrentPage, PAGE_SIZE,);
+  private readonly activePageRange = createPageRange(this.activeUsers, this.activeCurrentPage, PAGE_SIZE);
   readonly activeRangeStart = this.activePageRange.start;
   readonly activeRangeEnd = this.activePageRange.end;
 
-  private readonly bannedPageRange = createPageRange(this.bannedUsers, this.bannedCurrentPage, PAGE_SIZE,);
+  private readonly bannedPageRange = createPageRange(this.bannedUsers, this.bannedCurrentPage, PAGE_SIZE);
   readonly bannedRangeStart = this.bannedPageRange.start;
   readonly bannedRangeEnd = this.bannedPageRange.end;
 
